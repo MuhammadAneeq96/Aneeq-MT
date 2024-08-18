6d273c6b38c4d641a3c14939baad203b13ea04f6
aneeq_96 <ge78liz@mytum.de>
2024-08-18 16:24:04 +0200

    IWorkflowObject instead of IWorkflow as an argument

--- com.polarion.verifyCEVersionID/src/com/polarion/verifyCEVersionID/VerifyCEVersionID.java
+++ com.polarion.verifyCEVersionID/src/com/polarion/verifyCEVersionID/VerifyCEVersionID.java
@@ -12,6 +12,7 @@ import com.polarion.alm.tracker.model.ILinkRoleOpt;
 import com.polarion.alm.tracker.model.IModule;
 import com.polarion.alm.tracker.model.ILinkedWorkItemStruct;
 import com.polarion.alm.tracker.model.IWorkItem;
+import com.polarion.alm.tracker.model.IWorkflowObject;
 import com.polarion.alm.tracker.workflow.IArguments;
 import com.polarion.alm.tracker.workflow.ICallContext;
 import com.polarion.alm.tracker.workflow.IWorkflowCondition;
@@ -22,28 +23,28 @@ import java.util.Set;
 import java.util.List;
 
 
-public class VerifyCEVersionID implements ICustomWorkflowCondition<IWorkItem> {
+public class VerifyCEVersionID implements ICustomWorkflowCondition<IWorkflowObject> {
 	
 	//static final String CONDITION_NAME = Literals.WORKFLOW_PREFIX.getValue() + VerifyCEVersionID.class.getSimpleName();
 
-	IWorkItem currentWorkItem; // gets the current work item (CE)
+	IWorkflowObject currentWorkItem; // gets the current work item (CE)
 	Set<String> currentWorkItemID; // variable for storing the polarion ID of the current work item (CE)
 	Set<String> currentWorkItemVersionID; // variable for storing the value of the field 'Version' of the current work item (CE)
 	Set<String> linkedWorkItemtypeID; // variable for storing the type ID of linked work item types to the current work item (CE)
 	// Set<String> linkedTestCaseWorkItemtypeID; // variable for storing the type ID of linked work item types to the current work item (CE)
 	List<ILinkedWorkItemStruct> linkedTestCases;
 	Set<String> linkedWorkItemID; // variable for storing the ID of all linked work item types to the current work item (CE)
-	IWorkItem retrievedWorkItem;
+	IWorkflowObject retrievedWorkItem;
 	IModule retrievedCD;
 	boolean allTestsPassed;
 	boolean ceVersionMatch;
 	
 	//public void execute(ICallContext<IWorkItem> context, IArguments arguments) {
 	@Override
-	public String checkCondition(ICallContext<IWorkItem> context) {
+	public String checkCondition(ICallContext<IWorkflowObject> context) {
 	    IWorkItem currentWorkItem = context.getTarget();
 	    String currentWorkItemID = currentWorkItem.getId();
-	    Object currentWorkItemVersionID = currentWorkItem.getCustomField('ceVersion');
+	    Object currentWorkItemVersionID = currentWorkItem.getCustomField("ceVersion");
 	    allTestsPassed = true;
 		ceVersionMatch = true;
 		
