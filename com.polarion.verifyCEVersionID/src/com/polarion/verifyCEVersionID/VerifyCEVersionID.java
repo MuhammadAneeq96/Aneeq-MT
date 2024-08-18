a451b7a79d23d7a495f952a46a7e4063a4c52ff8
MuhammadAneeq96 <139444122+MuhammadAneeq96@users.noreply.github.com>
2024-08-19 00:17:31 +0200

    Update VerifyCEVersionID.java

--- com.polarion.verifyCEVersionID/src/com/polarion/verifyCEVersionID/VerifyCEVersionID.java
+++ com.polarion.verifyCEVersionID/src/com/polarion/verifyCEVersionID/VerifyCEVersionID.java
@@ -21,51 +21,93 @@ import com.tulrfsd.polarion.core.model.ICustomWorkflowCondition;
 //import com.tulrfsd.polarion.workflows.utils.Literals;
 import java.util.Set;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
-public class VerifyCEVersionID implements ICustomWorkflowCondition<IWorkflowObject> {
+public class VerifyCEVersionID implements ICustomWorkflowCondition<IWorkItem> {
 	
 	//static final String CONDITION_NAME = Literals.WORKFLOW_PREFIX.getValue() + VerifyCEVersionID.class.getSimpleName();
 
-	IWorkflowObject currentWorkItem; // gets the current work item (CE)
+	IWorkItem currentWorkItem; // gets the current work item (CE)
 	Set<String> currentWorkItemID; // variable for storing the polarion ID of the current work item (CE)
 	Set<String> currentWorkItemVersionID; // variable for storing the value of the field 'Version' of the current work item (CE)
 	Set<String> linkedWorkItemtypeID; // variable for storing the type ID of linked work item types to the current work item (CE)
 	// Set<String> linkedTestCaseWorkItemtypeID; // variable for storing the type ID of linked work item types to the current work item (CE)
 	List<ILinkedWorkItemStruct> linkedTestCases;
 	Set<String> linkedWorkItemID; // variable for storing the ID of all linked work item types to the current work item (CE)
-	IWorkflowObject retrievedWorkItem;
+	IWorkItem retrievedWorkItem;
 	IModule retrievedCD;
 	boolean allTestsPassed;
 	boolean ceVersionMatch;
 	
 	//public void execute(ICallContext<IWorkItem> context, IArguments arguments) {
 	@Override
-	public String checkCondition(ICallContext<IWorkflowObject> context) {
-	    IWorkItem currentWorkItem = context.getTarget();
+	public String checkCondition(ICallContext<IWorkItem> context) {
+		IWorkItem currentWorkItem = context.getTarget();
 	    String currentWorkItemID = currentWorkItem.getId();
 	    Object currentWorkItemVersionID = currentWorkItem.getCustomField("ceVersion");
 	    allTestsPassed = true;
 		ceVersionMatch = true;
 		
-	    // Object currentWorkItemVersionID = currentWorkItem.getValue('ceversion');
-	    
-	    currentWorkItem = context.getTarget();
-	    
-	    
-	    return false;
+        checkAllTestsPassed();
+        // condition2();
+			    
+	    //return false;
 	}
 	
-	 private boolean checkAllTestsPassed() {
+	 private boolean checkAllTestsPassed() {	 
+    	 List<ILinkedWorkItemStruct> linkedWorkItemsStructs = currentWorkItem.getLinkedWorkItemsStructBack();
+    	 List<IWorkItem> linkedTestCases = linkedWorkItemsStructs.stream()
+    	            .filter(linkedWorkItemStruct -> "tested_by".equals(linkedWorkItemStruct.getLinkRole().getId()))
+    	            .map(ILinkedWorkItemStruct::getLinkedItem)  // Map to the linked work item
+    	            .collect(Collectors.toList());  // Collect to a list
+    	 
+    	        //return linkedTestCases;
+    	        
+    	        for (IWorkItem testCase : linkedTestCases) {
+    	            Optional<TestRecord> mostRecentTestRecord = getMostRecentTestRecord(testCase);
+
+    	            if (mostRecentTestRecord.isPresent()) {
+    	                String testRecordStatus = mostRecentTestRecord.get().getCustomField(TestRecordFields.STATUS);
+
+    	                // Check if the status is not 'passed'
+    	                if (!"passed".equalsIgnoreCase(testRecordStatus)) {
+    	                    allTestsPassed = false;
+    	                    break;  // Exit the loop as soon as one failed test is found
+    	                }
+    	            } else {
+    	                // If there is no test record, consider it as a failed condition
+    	                allTestsPassed = false;
+    	                break;
+    	            }
+    	        }
+    	    }
+		 
+	 private Optional<TestRecord> getMostRecentTestRecord(IWorkItem testCase) {
+	        // Assuming testCase.getLinkedWorkItemsStructDirect() gives you the linked test records
+	        List<TestRecord> testRecords = testCase.getLinkedWorkItemsStructsDirect().stream()
+	                .filter(linkedWorkItemStruct -> linkedWorkItemStruct.getLinkedItem() instanceof TestRecord)
+	                .map(linkedWorkItemStruct -> (TestRecord) linkedWorkItemStruct.getLinkedItem())
+	                .collect(Collectors.toList());
+
+	        // Get the most recent test record based on the created date
+	        return testRecords.stream()
+	                .max((record1, record2) -> {
+	                    String created1 = record1.getCustomField(TestRecordFields.CREATED);
+	                    String created2 = record2.getCustomField(TestRecordFields.CREATED);
+	                    return created1.compareTo(created2);
+	                });
+	    }
 	
 		 // Use allTestsPassed here in the condition
+		 // checkAllTestsPassed = false;
 		 	 
-	 }
-	 
 	 private boolean checkCEVersionMatch() {
-		 
+		 //if...
+
  		 // module = workItem.getModule();
 		 // Use ceVersionMatch here in the condition
+		 checkAllTestsPassed = false;
 	 }
 	
 	
@@ -76,5 +118,4 @@ public class VerifyCEVersionID implements ICustomWorkflowCondition<IWorkflowObje
 		// TODO Auto-generated method stub
 		return null;
 	}*/
-
 }
